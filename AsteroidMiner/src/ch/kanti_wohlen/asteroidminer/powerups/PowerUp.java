9cf340ce252cb0370e34c0449a831c6ad1f01645
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-10-15 21:59:28 +0200

    Make methods onPickUp and getDropFrequency of PowerUp abstract.
    This forces sub-classes of PowerUp to override onPickUp and
    getDropFrequency. Empty methods for onPickUp or a drop frequency of 0
    would not make sense.
    And yay, your first code commit :D

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/PowerUp.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/PowerUp.java
@@ -7,16 +7,12 @@ import com.badlogic.gdx.physics.box2d.World;
 import ch.kanti_wohlen.asteroidminer.entities.Entity;
 
 public abstract class PowerUp extends Entity {
-
+	
 	public PowerUp(World world, BodyDef bodyDef, Shape collisionBox) {
 		super(world, bodyDef, collisionBox);
 	}
-
-	public void onPickUP() {
-
-	}
-
-	public float getDropFrequency() {
-		return 0;
-	}
+	
+	public abstract void onPickUp();
+	
+	public abstract float getDropFrequency();
 }
