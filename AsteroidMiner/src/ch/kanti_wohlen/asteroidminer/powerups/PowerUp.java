4330a7507989900900001c590f1085716e913230
Marco <marco.paolozzi@kanti-wohlen.ch>
2013-10-15 18:31:18 +0200

    Added Package powerups and Class PowerUp.

---
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/PowerUp.java
@@ -0,0 +1,22 @@
+package ch.kanti_wohlen.asteroidminer.powerups;
+
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+
+import ch.kanti_wohlen.asteroidminer.entities.Entity;
+
+public abstract class PowerUp extends Entity {
+
+	public PowerUp(World world, BodyDef bodyDef, Shape collisionBox) {
+		super(world, bodyDef, collisionBox);
+	}
+
+	public void onPickUP() {
+
+	}
+
+	public float getDropFrequency() {
+		return 0;
+	}
+}
