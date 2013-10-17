4130feec6e94b70ba98e868c8b354a4d2c45fe69
Marco <marco.paolozzi@kanti-wohlen.ch>
2013-10-17 17:47:26 +0200

    Added Player in PowerUp class and added LifePowerUp class.

---
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/LifePowerUp.java
@@ -0,0 +1,33 @@
+package ch.kanti_wohlen.asteroidminer.powerups;
+
+import ch.kanti_wohlen.asteroidminer.Player;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class LifePowerUp extends PowerUp {
+	
+	private static float Drop_Frequency;
+
+	public LifePowerUp(World world, BodyDef bodyDef, Shape collisionBox) {
+		super(world, bodyDef, collisionBox);
+	}
+
+	@Override
+	public void onPickUp(Player p) {
+
+	}
+
+	@Override
+	public float getDropFrequency() {
+		return Drop_Frequency;
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+
+	}
+
+}
--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/PowerUp.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/PowerUp.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 
+import ch.kanti_wohlen.asteroidminer.Player;
 import ch.kanti_wohlen.asteroidminer.entities.Entity;
 
 public abstract class PowerUp extends Entity {
@@ -12,7 +13,7 @@ public abstract class PowerUp extends Entity {
 		super(world, bodyDef, collisionBox);
 	}
 	
-	public abstract void onPickUp();
+	public abstract void onPickUp(Player p);
 	
 	public abstract float getDropFrequency();
 }
