87c0454916a141f3e9d70bad089146e0903c83fa
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-10-19 18:01:22 +0200

    Fixed Textures file formatting.

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/Textures.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/Textures.java
@@ -7,33 +7,33 @@ import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public final class Textures {
-	
+
 	public static Sprite BACKGROUND;
 	public static Sprite SPACESHIP;
 	public static Sprite ASTEROID;
 	public static Sprite LASER;
 	public static Sprite LIFEPOWERUPBOX;
-	
+
 	private Textures() {};
-	
+
 	/**
 	 * Loads the textures into memory.
-	 * Needs to be called before drawing any textures except the background
+	 * Needs to be called before drawing any textures.
 	 */
 	public static void load() {
 		Texture bg = new Texture(Gdx.files.internal("graphics/background.png"));
 		bg.setWrap(TextureWrap.Repeat, TextureWrap.Repeat);
 		BACKGROUND = new Sprite(bg, bg.getWidth(), bg.getHeight());
-		
+
 		Texture sprites = new Texture(Gdx.files.internal("graphics/sprites.png"));
 		SPACESHIP = new Sprite(sprites, 629, 34, 70, 81);
 		SPACESHIP.setOrigin(SPACESHIP.getWidth() / 2, SPACESHIP.getHeight() / 2);
 		SPACESHIP.getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
+
 		ASTEROID = new Sprite(sprites, 164, 237, 84, 83);
 		ASTEROID.setOrigin(ASTEROID.getWidth() / 2, ASTEROID.getHeight() / 2);
 		ASTEROID.getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
+
 		LASER = new Sprite(sprites, 364, 177, 7, 12);
 		LASER.setOrigin(LASER.getWidth() / 2, LASER.getHeight() / 2);
 		LASER.getTexture().setFilter(TextureFilter.Linear, TextureFilter.Linear);
