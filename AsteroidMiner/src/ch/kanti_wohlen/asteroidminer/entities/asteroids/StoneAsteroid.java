7ee5fe88b3e5dc377db530f0907ce66d62192e8a
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-11-08 16:39:28 +0100

    Made StoneAsteroid splitting a little more random.

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/asteroids/StoneAsteroid.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/asteroids/StoneAsteroid.java
@@ -21,7 +21,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 public class StoneAsteroid extends Entity implements Damageable {
 
 	public static final int MAX_HEALTH = 100;
-	public static final float STONE_ASTEROID_MIN_SIZE = 0.5f;
+	public static final float STONE_ASTEROID_MIN_SIZE = 0.75f;
 
 	private final HealthBar healthBar;
 	private final float currentRadius;
@@ -80,8 +80,8 @@ public class StoneAsteroid extends Entity implements Damageable {
 					return;
 				}
 
-				SplitAsteroidLauncher l = new SplitAsteroidLauncher(w, body.getPosition(),
-						body.getLinearVelocity(), nextRadius, body.getMass());
+				SplitAsteroidLauncher l = new SplitAsteroidLauncher(w, body.getPosition(), body.getLinearVelocity(),
+						nextRadius, body.getMass());
 				TaskScheduler.INSTANCE.runTask(l);
 			}
 		}
@@ -121,7 +121,7 @@ public class StoneAsteroid extends Entity implements Damageable {
 		fixture.shape = cs;
 		return fixture;
 	}
-	
+
 	private class SplitAsteroidLauncher implements Runnable {
 
 		private final World w;
@@ -140,19 +140,25 @@ public class StoneAsteroid extends Entity implements Damageable {
 
 		@Override
 		public void run() {
-			Vector2 locOff = new Vector2(velocity.cpy().nor());
-			Vector2 loc1 = position.cpy().add(locOff.rotate(90f));
-			Vector2 loc2 = position.cpy().add(locOff.rotate(180f));
+			final float r1 = radius * (0.75f + MathUtils.random() * 0.5f);
+			final float r2 = radius * (0.75f + MathUtils.random() * 0.5f);
+			final float rot1 = 75f + 30f * MathUtils.random();
+			final float rot2 = 255f + 30f * MathUtils.random();
 
-			Vector2 impulse = new Vector2(velocity);
-			impulse.scl(mass).scl(0.75f);
-			
-			StoneAsteroid a1 = new StoneAsteroid(w, loc1, radius, velocity);
-			StoneAsteroid a2 = new StoneAsteroid(w, loc2, radius, velocity);
-			
-			a1.getPhysicsBody().applyLinearImpulse(impulse.rotate(90f), position, true);
-			a2.getPhysicsBody().applyLinearImpulse(impulse.rotate(180f), position, true);
+			Vector2 loc1 = position.cpy().add(velocity.cpy().nor().rotate(rot1));
+			Vector2 loc2 = position.cpy().add(velocity.cpy().nor().rotate(rot2));
+
+			StoneAsteroid a1 = new StoneAsteroid(w, loc1, r1, velocity);
+			StoneAsteroid a2 = new StoneAsteroid(w, loc2, r2, velocity);
+
+			final float mass1 = a1.getPhysicsBody().getMass();
+			final float mass2 = a2.getPhysicsBody().getMass();
+			final Vector2 impulse = new Vector2(velocity);
+			final Vector2 i1 = impulse.cpy().div(mass).scl(5f * mass1 * mass1).rotate(90f);
+			final Vector2 i2 = impulse.cpy().div(mass).scl(5f * mass2 * mass2).rotate(270f);
+
+			a1.getPhysicsBody().applyLinearImpulse(i1, position, true);
+			a2.getPhysicsBody().applyLinearImpulse(i2, position, true);
 		}
-		
 	}
 }
