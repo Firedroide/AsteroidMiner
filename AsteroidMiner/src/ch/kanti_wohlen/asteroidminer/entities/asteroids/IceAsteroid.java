0c757dbd622588519fc56d01626c2796f4adc8fe
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-11-10 16:51:53 +0100

    Lower IceAsteroid max health.

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/asteroids/IceAsteroid.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/asteroids/IceAsteroid.java
@@ -19,7 +19,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class IceAsteroid extends Entity implements Damageable {
 
-	public static final int MAX_HEALTH = 200;
+	public static final int MAX_HEALTH = 150;
 
 	private final HealthBar healthBar;
 	private final Fixture circleFixture;
