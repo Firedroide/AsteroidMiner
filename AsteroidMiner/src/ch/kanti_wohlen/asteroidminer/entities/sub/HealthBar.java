05a58f18e45c6819989076d4db268420eb15c8f8
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-11-25 19:49:13 +0100

    Scale health bars according to maximum health.

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/sub/HealthBar.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/sub/HealthBar.java
@@ -10,11 +10,13 @@ public class HealthBar {
 
 	public static final float HEALTH_BAR_ALPHA_MAX = 5f;
 	private final float maxHealth;
+	private final float sizeModifier;
 	private float healthAlpha;
 
 	public HealthBar(float maximumHealth) {
 		maxHealth = maximumHealth;
 		healthAlpha = 0f;
+		sizeModifier = 0.6f + maximumHealth / 200f;
 	}
 
 	public void render(SpriteBatch batch, float health, Vector2 location) {
@@ -23,17 +25,20 @@ public class HealthBar {
 		healthAlpha = Math.max(0f, healthAlpha - Gdx.graphics.getDeltaTime());
 
 		final int xm = Math.round((float) health / maxHealth * Textures.HEALTH_HIGH.getRegionWidth());
+		final float xmScl = xm * sizeModifier;
+		final float xOff = Textures.HEALTH_HIGH.getRegionWidth() * ((1f - sizeModifier) * 0.5f);
 		final int xn = Textures.HEALTH_LOW.getRegionWidth() - xm;
 		final int wHigh = Textures.HEALTH_HIGH.getRegionWidth();
 		final int xLow = Textures.HEALTH_LOW.getRegionX();
 		final int wLow = Textures.HEALTH_LOW.getRegionWidth();
+		final float yHeight = Textures.HEALTH_HIGH.getRegionHeight() * sizeModifier;
 
-		Textures.HEALTH_HIGH.setBounds(location.x, location.y, xm, Textures.HEALTH_HIGH.getHeight());
+		Textures.HEALTH_HIGH.setBounds(location.x + xOff, location.y, xmScl, yHeight);
 		Textures.HEALTH_HIGH.setRegionWidth(xm);
 		Textures.HEALTH_HIGH.draw(batch, Math.min(healthAlpha, 1f));
 		Textures.HEALTH_HIGH.setRegionWidth(wHigh);
 
-		Textures.HEALTH_LOW.setBounds(location.x + xm, location.y, xn, Textures.HEALTH_LOW.getHeight());
+		Textures.HEALTH_LOW.setBounds(location.x + xOff + xmScl, location.y, xn * sizeModifier, yHeight);
 		Textures.HEALTH_LOW.setRegionX(xLow + xm);
 		Textures.HEALTH_LOW.setRegionWidth(xn);
 		Textures.HEALTH_LOW.draw(batch, Math.min(healthAlpha, 1f));
