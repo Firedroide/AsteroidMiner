038cff7f76901ff72c76f98328f21ba13bd04ffe
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-12-01 21:53:09 +0100

    Improved StoneAsteroid splitting again. Hopefully the last time now :/

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/asteroids/StoneAsteroid.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/asteroids/StoneAsteroid.java
@@ -160,23 +160,26 @@ public class StoneAsteroid extends Entity implements Damageable {
 		public void run() {
 			final float r1 = radius * (0.75f + MathUtils.random() * 0.5f);
 			final float r2 = radius * (0.75f + MathUtils.random() * 0.5f);
-			final float rot1 = 75f + 30f * MathUtils.random();
-			final float rot2 = 255f + 30f * MathUtils.random();
+			final float rot0 = velocity.angle() * MathUtils.degreesToRadians;
+			final float rot1 = MathUtils.random(MathUtils.PI * 0.4f, MathUtils.PI * 0.6f);
+			final float rot2 = MathUtils.random(MathUtils.PI * 1.4f, MathUtils.PI * 1.6f);
 
-			Vector2 loc1 = position.cpy().add(-MathUtils.sin(rot1) * r1 * 2f, MathUtils.cos(rot1) * r1 * 2f);
-			Vector2 loc2 = position.cpy().add(-MathUtils.sin(rot2) * r2 * 2f, MathUtils.cos(rot2) * r2 * 2f);
+			Vector2 loc1 = position.cpy().add(MathUtils.cos(rot0 + rot1) * r1 * 1.1f,
+					MathUtils.sin(rot0 + rot1) * r1 * 1.1f);
+			Vector2 loc2 = position.cpy().add(MathUtils.cos(rot0 + rot2) * r2 * 1.1f,
+					MathUtils.sin(rot0 + rot2) * r2 * 1.1f);
 
 			StoneAsteroid a1 = new StoneAsteroid(w, loc1, r1, velocity);
 			StoneAsteroid a2 = new StoneAsteroid(w, loc2, r2, velocity);
 
 			final float mass1 = a1.getPhysicsBody().getMass();
 			final float mass2 = a2.getPhysicsBody().getMass();
-			final Vector2 impulse = new Vector2(velocity);
-			final Vector2 i1 = impulse.cpy().div(mass).scl(5f * mass1 * mass1).rotate(90f);
-			final Vector2 i2 = impulse.cpy().div(mass).scl(5f * mass2 * mass2).rotate(270f);
+			final Vector2 impulse = new Vector2(velocity.div(mass));
+			final Vector2 i1 = impulse.cpy().scl(50f * mass1 * mass1).rotate(rot1 * MathUtils.radiansToDegrees);
+			final Vector2 i2 = impulse.cpy().scl(50f * mass2 * mass2).rotate(rot2 * MathUtils.radiansToDegrees);
 
-			a1.getPhysicsBody().applyLinearImpulse(i1, position, true);
-			a2.getPhysicsBody().applyLinearImpulse(i2, position, true);
+			a1.getPhysicsBody().applyForceToCenter(i1, true);
+			a2.getPhysicsBody().applyForceToCenter(i2, true);
 		}
 	}
 }
