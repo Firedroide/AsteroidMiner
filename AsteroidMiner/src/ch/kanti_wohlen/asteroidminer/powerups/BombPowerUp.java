e33aebf17d9d2207fed9f13da92376d3da891292
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-12-07 23:43:31 +0100

    Make BombPowerUp use new Explosion animation.

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/BombPowerUp.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/powerups/BombPowerUp.java
@@ -2,15 +2,12 @@ package ch.kanti_wohlen.asteroidminer.powerups;
 
 import ch.kanti_wohlen.asteroidminer.Player;
 import ch.kanti_wohlen.asteroidminer.Textures;
-import ch.kanti_wohlen.asteroidminer.entities.Entity;
-import ch.kanti_wohlen.asteroidminer.entities.EntityType;
+import ch.kanti_wohlen.asteroidminer.animations.Explosion;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 
 public class BombPowerUp extends PowerUp {
 
@@ -23,17 +20,7 @@ public class BombPowerUp extends PowerUp {
 
 	@Override
 	public void onPickUp(Player player) {
-		Array<Body> bodies = new Array<Body>(world.getBodyCount());
-		world.getBodies(bodies);
-
-		for (Body b : bodies) {
-			if (b == null) continue;
-			Entity e = (Entity) b.getUserData();
-
-			if (e.getType() == EntityType.ASTEROID) {
-				e.remove();
-			}
-		}
+		new Explosion(world, body.getPosition().cpy(), 50f, 100, false);
 	}
 
 	@Override
