05c9e65e285540bbb7470033fef6ddf2337c634f
Roger Baumgartner <rogermarcbaumgartner@hotmail.com>
2013-12-29 03:58:34 +0100

    Implement Fading in Entity.

--- AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/Entity.java
+++ AsteroidMiner/src/ch/kanti_wohlen/asteroidminer/entities/Entity.java
@@ -1,7 +1,7 @@
 package ch.kanti_wohlen.asteroidminer.entities;
 
-import ch.kanti_wohlen.asteroidminer.TaskScheduler;
-import ch.kanti_wohlen.asteroidminer.TaskScheduler.Task;
+import ch.kanti_wohlen.asteroidminer.fading.FadeOutHelper;
+import ch.kanti_wohlen.asteroidminer.fading.Fadeable;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,7 +14,7 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
-public abstract class Entity {
+public abstract class Entity implements Fadeable {
 
 	public static final float BOX2D_TO_PIXEL = 10f;
 	public static final float PIXEL_TO_BOX2D = 0.1f;
@@ -44,6 +44,9 @@ public abstract class Entity {
 	public boolean remove() {
 		if (isRemoved()) return false;
 		removed = true;
+		if (fadeOut != null) {
+			fadeOut.cancel();
+		}
 		return true;
 	}
 
@@ -59,9 +62,23 @@ public abstract class Entity {
 		fadeOut(0f, fadeOutTime);
 	}
 
+	public float getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(float newAlpha) {
+		alpha = MathUtils.clamp(newAlpha, 0f, 1f);
+	}
+
 	public void fadeOut(float startDelay, float fadeOutTime) {
 		if (fadeOut != null) return; // If already fading out, ignore
-		fadeOut = new FadeOutHelper(startDelay, fadeOutTime);
+		fadeOut = new FadeOutHelper(this, startDelay, fadeOutTime, new Runnable() {
+
+			@Override
+			public void run() {
+				remove();
+			}
+		});
 	}
 
 	protected void positionSprite(Sprite sprite) {
@@ -77,29 +94,4 @@ public abstract class Entity {
 	public abstract EntityType getType();
 
 	public abstract Rectangle getBoundingBox();
-
-	private class FadeOutHelper implements Runnable {
-
-		private final float time;
-		private final Task task;
-		private float currentTime;
-
-		private FadeOutHelper(float startDelay, float fadeOutTime) {
-			time = fadeOutTime;
-			task = TaskScheduler.INSTANCE.runTaskRepeated(this, startDelay, 0, time);
-		}
-
-		@Override
-		public void run() {
-			if (isRemoved()) {
-				task.cancel();
-				return;
-			}
-			currentTime += 1 * TaskScheduler.TICK_TIME;
-			alpha = 1f - currentTime / time;
-			if (time - currentTime < TaskScheduler.TICK_TIME) {
-				remove();
-			}
-		}
-	}
 }
